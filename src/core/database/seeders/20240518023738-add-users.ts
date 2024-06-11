61740d21e2678ad36f4e81f7ddfb2926cd6cc472
Keila Fernandes <keiladaianefernandes24@gmail.com>
2024-06-11 19:05:41 -0300

    fix: move format user to utils folder

--- src/core/database/seeders/20240518023738-add-users.ts
+++ src/core/database/seeders/20240518023738-add-users.ts
@@ -1,122 +1,25 @@
 'use strict';
 
-import users from './data/users.json';
+import usersToAdd from './data/users.json';
 import { QueryInterface } from 'sequelize';
-import { CreateUserDto } from 'src/modules/user/dto/create-user.dto';
-import { validate } from 'class-validator';
-import { plainToClass } from 'class-transformer';
-import { convertToSnakeCase } from 'src/common/helpers/object.helper';
-import { removeNonNumericCharacters } from 'src/common/helpers/string.helper';
-import { parseDateFromString } from 'src/common/helpers/date.helper';
-import { BCRYPT_SALT } from 'src/common/constants/auth';
-import bcrypt from 'bcryptjs';
-import { USER_GENDER } from 'src/modules/user/enums/user-gender.enum';
-import { USER_ZODIAC_SIGN } from 'src/modules/user/enums/user-zodiac-sign.enum';
+import { formatUsers } from './utils/formatUser';
 
-/** @type {import("sequelize-cli").Migration} */
+/** @type {import("sequelize-cli").Seed} */
 module.exports = {
   up: (queryInterface: QueryInterface): Promise<void> =>
     queryInterface.sequelize.transaction(async (transaction) => {
-      const promises = users.map(async (item) => {
-        // already exist CPF
-        const alreadyExistsUser = await queryInterface.select(null, 'users', {
-          where: { cpf: removeNonNumericCharacters(item.cpf) },
-        });
+      const [users, addresses, physicals] = await formatUsers(
+        usersToAdd,
+        queryInterface,
+      );
 
-        if (alreadyExistsUser.length > 0)
-          return console.error('User already exists in database');
-
-        const gender = {
-          feminino: USER_GENDER.FEMALE,
-          masculino: USER_GENDER.MALE,
-        };
-
-        const zodiacSign = {
-          áries: USER_ZODIAC_SIGN.ARIES,
-          touro: USER_ZODIAC_SIGN.TAURUS,
-          gêmeos: USER_ZODIAC_SIGN.GEMINI,
-          câncer: USER_ZODIAC_SIGN.CANCER,
-          leão: USER_ZODIAC_SIGN.LEO,
-          virgem: USER_ZODIAC_SIGN.VIRGO,
-          libra: USER_ZODIAC_SIGN.LIBRA,
-          escorpião: USER_ZODIAC_SIGN.SCORPIO,
-          sagitário: USER_ZODIAC_SIGN.SAGITTARIUS,
-          capricórnio: USER_ZODIAC_SIGN.CAPRICORN,
-          aquário: USER_ZODIAC_SIGN.AQUARIUS,
-          peixes: USER_ZODIAC_SIGN.PISCES,
-        };
-
-        const newUser = {
-          name: item.nome,
-          cpf: removeNonNumericCharacters(item.cpf),
-          rg: removeNonNumericCharacters(item.rg),
-          birthdate: parseDateFromString(item.data_nasc).toUTCString(),
-          gender: gender[item.sexo.toLowerCase()],
-          zodiacSign: zodiacSign[item.signo.toLowerCase()],
-          motherName: item.mae,
-          fatherName: item.pai,
-          email: item.email,
-          password: await bcrypt.hash(item.senha, BCRYPT_SALT),
-          phone: removeNonNumericCharacters(item.celular),
-          cellphone: removeNonNumericCharacters(item.telefone_fixo),
-          address: {
-            cep: removeNonNumericCharacters(item.cep),
-            street: item.endereco,
-            number: Number(item.numero),
-            neighborhood: item.bairro,
-            city: item.cidade,
-            state: item.estado,
-          },
-          physical: {
-            height: Number(item.altura.replace(',', '.')),
-            weight: Number(item.peso),
-            bloodType: item.tipo_sanguineo,
-          },
-        };
-
-        const isValid = await validate(
-          plainToClass(CreateUserDto, newUser),
-        ).then(async (errors) => {
-          if (errors.length > 0) {
-            console.error('validation failed. errors: ', errors);
-            return false;
-          }
-          return true;
-        });
-
-        if (!isValid) return;
-
-        const { address, physical, ...user } = newUser;
-
-        const [user_id] = (await queryInterface.insert(
-          null,
-          'users',
-          {
-            ...convertToSnakeCase(user),
-            created_at: new Date().toUTCString(),
-            updated_at: new Date().toUTCString(),
-          },
-          {
-            transaction,
-          },
-        )) as unknown as number[];
-
-        await queryInterface.insert(
-          null,
-          'addresses',
-          convertToSnakeCase({ ...address, user_id }),
-          { transaction },
-        );
-
-        await queryInterface.insert(
-          null,
-          'physicals',
-          convertToSnakeCase({ ...physical, user_id }),
-          { transaction, mapToModel: true },
-        );
+      await queryInterface.bulkInsert('users', users, { transaction });
+      await queryInterface.bulkInsert('addresses', addresses, {
+        transaction,
       });
-
-      await Promise.all(promises);
+      await queryInterface.bulkInsert('physicals', physicals, {
+        transaction,
+      });
     }),
 
   down: (queryInterface: QueryInterface): Promise<void> =>
---
+++ src/core/database/seeders/utils/formatUser.ts
@@ -0,0 +1,104 @@
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { BCRYPT_SALT } from 'src/common/constants/auth';
+import { parseDateFromString } from 'src/common/helpers/date.helper';
+import { removeNonNumericCharacters } from 'src/common/helpers/string.helper';
+import { CreateUserDto } from 'src/modules/user/dto/create-user.dto';
+import { USER_GENDER } from 'src/modules/user/enums/user-gender.enum';
+import { USER_ZODIAC_SIGN } from 'src/modules/user/enums/user-zodiac-sign.enum';
+import bcrypt from 'bcryptjs';
+import { convertToSnakeCase } from 'src/common/helpers/object.helper';
+
+const gender = {
+  feminino: USER_GENDER.FEMALE,
+  masculino: USER_GENDER.MALE,
+};
+
+const zodiacSign = {
+  áries: USER_ZODIAC_SIGN.ARIES,
+  touro: USER_ZODIAC_SIGN.TAURUS,
+  gêmeos: USER_ZODIAC_SIGN.GEMINI,
+  câncer: USER_ZODIAC_SIGN.CANCER,
+  leão: USER_ZODIAC_SIGN.LEO,
+  virgem: USER_ZODIAC_SIGN.VIRGO,
+  libra: USER_ZODIAC_SIGN.LIBRA,
+  escorpião: USER_ZODIAC_SIGN.SCORPIO,
+  sagitário: USER_ZODIAC_SIGN.SAGITTARIUS,
+  capricórnio: USER_ZODIAC_SIGN.CAPRICORN,
+  aquário: USER_ZODIAC_SIGN.AQUARIUS,
+  peixes: USER_ZODIAC_SIGN.PISCES,
+};
+
+export const formatUsers = async (allUsers, queryInterface) => {
+  const [users, addresses, physicals] = await allUsers.reduce(
+    async (acc, item, index) => {
+      const [usersAcc, addressesAcc, physicalInfoAcc] = await acc;
+
+      // already exist CPF
+      const alreadyExistsUser = await queryInterface.select(null, 'users', {
+        where: { cpf: removeNonNumericCharacters(item.cpf) },
+      });
+
+      if (alreadyExistsUser.length > 0) {
+        console.error('User already exists in database');
+        return [usersAcc, addressesAcc, physicalInfoAcc];
+      }
+
+      const id = Number(index + 1);
+
+      const newUser = {
+        id,
+        name: item.nome,
+        cpf: removeNonNumericCharacters(item.cpf),
+        rg: removeNonNumericCharacters(item.rg),
+        birthdate: parseDateFromString(item.data_nasc).toUTCString(),
+        gender: gender[item.sexo.toLowerCase()],
+        zodiacSign: zodiacSign[item.signo.toLowerCase()],
+        motherName: item.mae,
+        fatherName: item.pai,
+        email: item.email,
+        password: await bcrypt.hash(item.senha, BCRYPT_SALT),
+        phone: removeNonNumericCharacters(item.celular),
+        cellphone: removeNonNumericCharacters(item.telefone_fixo),
+      };
+
+      const address = {
+        cep: removeNonNumericCharacters(item.cep),
+        street: item.endereco,
+        number: Number(item.numero),
+        neighborhood: item.bairro,
+        city: item.cidade,
+        state: item.estado,
+        userId: id,
+      };
+
+      const physical = {
+        height: Number(item.altura.replace(',', '.')),
+        weight: Number(item.peso),
+        bloodType: item.tipo_sanguineo,
+        userId: id,
+      };
+
+      const isValidUser = await validate(
+        plainToClass(CreateUserDto, { ...newUser, address, physical }),
+      ).then(async (errors) => {
+        if (errors.length > 0) {
+          console.error('validation failed. errors: ', errors);
+          return false;
+        }
+        return true;
+      });
+
+      if (!isValidUser) return;
+
+      return [
+        [...usersAcc, convertToSnakeCase(newUser)],
+        [...addressesAcc, convertToSnakeCase(address)],
+        [...physicalInfoAcc, convertToSnakeCase(physical)],
+      ];
+    },
+    Promise.resolve([[], [], []]),
+  );
+
+  return [users, addresses, physicals];
+};
